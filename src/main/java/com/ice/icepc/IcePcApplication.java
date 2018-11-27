2bca2048fe93946d71b2477ae2b8bf359cb99985
Aditya Bhavaraju <adityateja.b@gmail.com>
2018-11-27 10:54:01 +0530

    Initial commit project structure

---
+++ src/main/java/com/ice/icepc/IcePcApplication.java
@@ -0,0 +1,12 @@
+package com.ice.icepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcePcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcePcApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ice/icepc/IcePcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ice.icepc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IcePcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
